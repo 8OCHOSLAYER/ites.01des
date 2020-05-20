82f8a0ff48b1b803a19fe3965764ba8144fb808c
8OCHOSLAYER <61218831+8OCHOSLAYER@users.noreply.github.com>
2020-05-19 19:31:00 -0500

    Inclucion del sistema
    se incluye la primera fase del alfa para la aplicacion

---
+++ views/index.php
@@ -0,0 +1,21 @@
+<?php include 'partials/head.php';?>
+<?php include 'partials/menu.php';?>
+
+<div class="container">
+
+	<div class="starter-template">
+		<br>
+		<br>
+		<br>
+		<div class="jumbotron">
+			<div class="container">
+
+					<a href="login.php" class="btn btn-primary btn-lg">Login</a>
+				</p>
+			</div>
+		</div>
+	</div>
+
+</div><!-- /.container -->
+
+<?php include 'partials/footer.php';?>
